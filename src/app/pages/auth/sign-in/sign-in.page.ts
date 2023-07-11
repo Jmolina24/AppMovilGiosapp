10bc5a4aec1e996f38e0f4241c5350a4d94d8f75
JairMolina <jairmolina51@gmail.com>
2023-07-11 11:38:54 -0500

    Actualizando Desgin del Login

--- src/app/pages/auth/sign-in/sign-in.page.ts
+++ src/app/pages/auth/sign-in/sign-in.page.ts
@@ -2,6 +2,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { Alert } from 'src/app/components/alert/alert.component';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { StorageService } from 'src/app/core/services/storage.service';
@@ -14,18 +15,18 @@ import { StorageService } from 'src/app/core/services/storage.service';
 export class SignInPage implements OnInit {
 	screen: any = 'signin';
 	formData: FormGroup;
-
 	alert!: Alert;
-
 	isLoading: boolean = false;
-
 	showPassword: boolean = false;
+	isAlertOpen = false;
+	public alertButtons = ['OK'];
 
 	constructor(
 		private fb: FormBuilder,
 		private auth: AuthService,
 		private _storage: StorageService,
-		private _router: Router
+		private _router: Router,
+		private alertController: AlertController
 	) {
 		this.formData = this.fb.group({
 			username: ['', [Validators.required]],
@@ -33,49 +34,52 @@ export class SignInPage implements OnInit {
 		});
 	}
 
-	ngOnInit() {}
+	ngOnInit() { 
+
+		
+	}
+
+	async presentAlert(message:any) {
+		const alert = await this.alertController.create({
+		  header: 'Notificación Giosapp',
+		  subHeader: '',
+		  message: message,
+		  buttons: ['OK'],
+		});
+	
+		await alert.present();
+	  }
+
+
 
 	signIn(): void {
 		if (this.formData.invalid) {
+			this.presentAlert('Completar todos los campos');
 			return;
 		}
 		this.isLoading = true;
-
 		this.formData.disable();
-
 		this.auth.signIn(this.formData.value).subscribe(
+			
 			(response: any) => {
-				this.alert = {
-					type: response.codigo === 0 ? 'success' : 'error',
-					message: response.mensaje,
-				};
-
-				if (response.codigo !== 0) {
+				// this.setOpen(true)
+				console.log('********************************************************');
+				console.log(response.error);
+				if (response.codigo == 1) {
+					this.presentAlert(response.mensaje );
 					return;
+				} else {
+					const { token, id, idrol, ...user } = response;
+
+					this._storage.saveToken(token);
+					this._storage.saveUser(user);
+					this._storage.saveUserId(id);
+					this._storage.saveRolId(idrol);
+
+					if (this._storage.getUserId()) {
+						this._router.navigate(['/manage-orders']);
+					}
 				}
-				const { token, id, idrol, ...user } = response;
-
-				this._storage.saveToken(token);
-				this._storage.saveUser(user);
-				this._storage.saveUserId(id);
-				this._storage.saveRolId(idrol);
-
-				if (this._storage.getUserId()) {
-					this._router.navigate(['/manage-orders']);
-				}
-			},
-			(error: HttpErrorResponse) => {
-				this.alert = {
-					title: error.name,
-					message: error.error.mensaje || error.message,
-					type: 'error',
-				};
-
-				this.formData.enable();
-				this.isLoading = false;
-			},
-			() => {
-				this.isLoading = false;
 			}
 		);
 	}
