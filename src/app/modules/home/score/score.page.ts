05a32c042e68bfae7a47f91cdd85e08e5dd10b05
JairMolina <jairmolina51@gmail.com>
2023-07-24 23:53:31 -0500

    se realiza ajuste del score

--- src/app/modules/home/score/score.page.ts
+++ src/app/modules/home/score/score.page.ts
@@ -12,6 +12,8 @@ import { StorageService } from 'src/app/core/services/storage.service';
 export class ScorePage implements OnInit {
 
   idtercero: string = this._storage.getUser().idtercero;
+  idcliente: string = this._storage.getUser().idcliente;
+  idclientesede: string = this._storage.getUser().idclientesede;
   _data: ScoreAPP[] = [];
   _resultado: any = {};
 
@@ -23,6 +25,7 @@ export class ScorePage implements OnInit {
 
 
   ngOnInit() {
+    console.log(this.idclientesede);
     // this._resultado = {
     //   "asignadas": 0,
     //   "pasignar": 0,
@@ -34,7 +37,7 @@ export class ScorePage implements OnInit {
   }
 
   handleRefresh(event: any) {
-    this._api.getListScore({ idcliente: 0, idclientesede: 0, idtercero: this.idtercero }).subscribe((r) => {
+    this._api.getListScore({ idcliente: this.idcliente, idclientesede: this.idclientesede, idtercero: this.idtercero }).subscribe((r) => {
       this._data = r;
       const objetoResultado: { [key: string]: number } = {};
       for (const elemento of this._data) {
@@ -50,7 +53,7 @@ export class ScorePage implements OnInit {
 
 
   get(): void {
-    this._api.getListScore({ idcliente: 0, idclientesede: 0, idtercero: this.idtercero }).subscribe((r) => {
+    this._api.getListScore({ idcliente: this.idcliente, idclientesede: this.idclientesede, idtercero: this.idtercero }).subscribe((r) => {
       this._data = r;
       const objetoResultado: { [key: string]: number } = {};
       for (const elemento of this._data) {
